fe85c075213ab259225c51f56c63f40f7686839a
jukramer <justas.kramer@gmail.com>
2025-12-09 19:16:03 +0100

    updates

--- 3_2/calc.py
+++ 3_2/calc.py
@@ -4,26 +4,34 @@ import scipy as sp
 
 
 class Calc:
-    def __init__(self):
-        return
+    def __init__(self, airfoilDatFile):
+        self.airfoilUpper = sp.interpolate.interp1d(1, 1) # TODO: Interpolate airfoil dat file
+        self.airfoilLower = sp.interpolate.interp1d(1, 1)
     
-    def calcAero(self, xVals, yVals, cpLowerVals, cpUpperVals, V1Vals, VInf, pInf, rho, alpha):
-        # Useful values
+    def calcAero(self, xVals, y1Vals, cpLowerVals, cpUpperVals, V1Vals, alpha, VInf, pInf, rho, pressureDrag=True):
         alpha = np.deg2rad(alpha)
         c = np.max(xVals)
         qInf = 0.5*rho*VInf**2
-        pInfArray = np.full_like(yVals, pInf)
-        VInfArray = np.full_like(yVals, VInf)
-        
+        pInfArray = np.full_like(y1Vals, pInf)
+        VInfArray = np.full_like(y1Vals, VInf)
+                
         # Pressure values
         pLowerVals = cpLowerVals * qInf + np.full_like(cpLowerVals, pInf) # Lower airfoil surface
         pUpperVals = cpUpperVals * qInf + np.full_like(cpLowerVals, pInf) # Upper airfoil surface
         p1Vals = None # TODO find pressure values
         
-        # Forces and Moments
+        # Normal and Axial Force
         N = sp.integrate.simpson(xVals, pLowerVals) - sp.integrate.simpson(xVals, pUpperVals)
-        D = rho*sp.integrate.simpson(yVals, V1Vals*(VInfArray - V1Vals)) + sp.integrate.simpson(yVals, pInfArray-p1Vals)
-        L = N*(np.cos(alpha) + np.sin(alpha)**2/np.cos(alpha)) - D*np.tan(alpha)
+        A = sp.integrate.simpson(self.airfoilLower(xVals), pLowerVals) - sp.integrate.simpson(self.airfoilUpper(xVals), pUpperVals)
+        
+        # Lift and Drag
+        L = N*np.cos(alpha) - A*np.sin(alpha)
+        if pressureDrag:
+            D = N*np.sin(alpha) + A*np.cos(alpha)
+        else:    
+            D = rho*sp.integrate.simpson(y1Vals, V1Vals*(VInfArray - V1Vals)) + sp.integrate.simpson(y1Vals, pInfArray-p1Vals)
+
+        # Leading Edge Moment
         M = sp.integrate.simpson(x=xVals, y=(pUpperVals - pLowerVals)*xVals) # Leading edge moment!
         
         # Lift, Drag, Moment, cl, cd, cm
